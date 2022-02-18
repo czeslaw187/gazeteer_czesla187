bae9b90418299045ad5a7b464af62944c5c16110
czeslaw187 <66272563+czeslaw187@users.noreply.github.com>
2022-02-18 12:31:04 +0000

    redux installation and added logics

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,21 @@
+import Layout from '../components/Layout'
+import {createStore, applyMiddleware} from 'redux'
+import {Provider} from 'react-redux'
+import reducer from '../lib/reducer.js'
+import thunk from 'redux-thunk'
 import '../styles/globals.css'
 
-function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
-}
+export const store = createStore(reducer, applyMiddleware(thunk))
 
-export default MyApp
+export default function MyApp({
+  Component,
+  pageProps: { session, ...pageProps },
+}) {
+  return (
+    <Provider store={store}>
+      <Layout>
+        <Component {...pageProps} />
+      </Layout>
+    </Provider>
+  )
+}
