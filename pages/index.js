ce6e2629c6aa6340e0fa867f525828a72fd53df4
czeslaw187 <66272563+czeslaw187@users.noreply.github.com>
2022-03-12 12:10:39 +0000

    destructured props

--- pages/index.js
+++ pages/index.js
@@ -4,36 +4,37 @@ import {connect} from 'react-redux';
 import * as actionCreator from '../lib/actions.js';
 
 function Home(props) {  
+  const {state, loadCountries, loadCoords, loadPoly, loadInfo} = props
   const [coordinates,setCoordinates] = useState([]) 
   const MapWithNoSSR = dynamic(() => import("../components/MyMap"), {
     ssr: false
   });
-  let storeData = props.state ? props.state.mapData : false
+  let storeData = state ? state.mapData : false
 
   useEffect(()=>{
     navigator.geolocation.getCurrentPosition(position=>{
       setCoordinates([position.coords.latitude,position.coords.longitude]) 
     })
-    props.loadCountries()
-  },[props])
+    loadCountries()
+  },[])
 
   useEffect(()=>{
     if (coordinates.length > 0) {
-      props.loadCoords(coordinates)
+      loadCoords(coordinates)
     }
   },[coordinates])
 
   useEffect(()=>{
     if (storeData) {
-      props.loadPoly(storeData[0].data)
-      props.loadInfo(storeData[0].data)
+      loadPoly(storeData[0].data)
+      loadInfo(storeData[0].data)
     }
   },[storeData[0]])
   
   return (
     <main>
       <div id="map" className="w-screen h-screen">
-        <MapWithNoSSR key="mymap" latLng={coordinates} coords={props}/>
+        <MapWithNoSSR key="mymap" latLng={coordinates} coords={state}/>
       </div>
     </main>
   );
