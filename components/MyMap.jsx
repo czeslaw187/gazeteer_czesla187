ab4d868de4aeb2e74bfefa2ea19e4a7753256714
czeslaw187 <66272563+czeslaw187@users.noreply.github.com>
2022-04-10 22:39:55 +0100

    placed side menu inside separate component

--- components/MyMap.jsx
+++ components/MyMap.jsx
@@ -4,7 +4,8 @@ import {useState} from 'react'
 import "leaflet/dist/leaflet.css";
 import "leaflet-defaulticon-compatibility/dist/leaflet-defaulticon-compatibility.css";
 import "leaflet-defaulticon-compatibility";
-import Weather from '../components/Weather.js'
+import Weather from './Weather.js'
+import SideMenu from "./SideMenu.js";
 
 function MyMap({latLng, coords}) {
   const [mymap,setMymap] = useState(null)
@@ -66,20 +67,7 @@ function MyMap({latLng, coords}) {
         }) : ['No content']
       }
       <LayersControl.Overlay>
-        <div className={sideMenu == true ? 
-                        "max-w-full md:max-w-xl transition-slider relative top-14 h-5/6 bg-sky-200 z-[9999] border-2 border-gray-800 rounded-sm px-1 overflow-x-auto" : 
-                        "max-w-0 transition-slider absolute h-5/6 top-14 z-[9999] border-2 border-gray-800 rounded-sm px-0 overflow-x-auto"}>
-          <button className="w-[15] h-auto text-lg pl-3 font-bold relative left-console underline text-blue-600" onClick={()=>{setSideMenu(!sideMenu)}} >{"<<"}</button>       
-          <h1 className="text-2xl mt-7 mb-5 text-center">{country.countryData?.data?.name}</h1>
-          <hr/>
-          <img src={country.countryData?.data?.flag} alt="flag" className="mr-auto ml-5 border-2 border-gray-900 w-6/12 h-auto" /> 
-          <hr/>
-          <p className="text-lg font-bold mx-6 my-5">Capital: {country.countryData?.data?.capital}</p>  
-          <hr/>
-          <p className="text-lg font-bold mx-6 my-5">Population: {country.countryData?.data?.population}</p>  
-          <hr/>
-          <div dangerouslySetInnerHTML={{__html: country.wikipedia?.data?.content}} /><a href={country.wikipedia?.data?.wikiUrl}>{country.wikipedia?.data?.content ? 'more...' : 'No content'}</a>
-        </div>
+        <SideMenu country={country} sideMenu={sideMenu} setSideMenu={setSideMenu} />
         <div>
           <button className="border-gray-900 border-2 z-[9998] rounded-md p-3 h-14 absolute top-20 left-2 font-bold text-lg text-left bg-white" onClick={()=>{setSideMenu(!sideMenu)}}>{">>"}</button>
           <button className="absolute top-40 border-gray-900 rounded-md border-2 p-2 h-14 z-[9998] left-2 bg-white" onClick={()=>{setWeather(!weather)}}>
---
+++ components/SideMenu.js
@@ -0,0 +1,20 @@
+function SideMenu({country, sideMenu, setSideMenu}) {
+    return ( 
+        <div className={sideMenu == true ? 
+            "max-w-full md:max-w-xl transition-slider relative top-14 h-5/6 bg-sky-200 z-[9999] border-2 border-gray-800 rounded-sm px-1 overflow-x-auto" : 
+            "max-w-0 transition-slider absolute h-5/6 top-14 z-[9999] border-2 border-gray-800 rounded-sm px-0 overflow-x-auto"}>
+            <button className="w-[15] h-auto text-lg pl-3 font-bold relative left-console underline text-blue-600" onClick={()=>{setSideMenu(!sideMenu)}} >{"<<"}</button>       
+            <h1 className="text-2xl mt-7 mb-5 text-center">{country.countryData?.data?.name}</h1>
+            <hr/>
+            <img src={country.countryData?.data?.flag} alt="flag" className="mr-auto ml-5 border-2 border-gray-900 w-6/12 h-auto" /> 
+            <hr/>
+            <p className="text-lg font-bold mx-6 my-5">Capital: {country.countryData?.data?.capital}</p>  
+            <hr/>
+            <p className="text-lg font-bold mx-6 my-5">Population: {country.countryData?.data?.population}</p>  
+            <hr/>
+            <div dangerouslySetInnerHTML={{__html: country.wikipedia?.data?.content}} /><a href={country.wikipedia?.data?.wikiUrl}>{country.wikipedia?.data?.content ? 'more...' : 'No content'}</a>
+        </div>
+     );
+}
+
+export default SideMenu;
