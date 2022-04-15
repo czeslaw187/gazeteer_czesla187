5221a4e7fb8268a7124db45cb909925e87a40ac7
czeslaw187 <66272563+czeslaw187@users.noreply.github.com>
2022-04-15 22:14:04 +0100

    https for http

--- pages/api/getGeoJson.js
+++ pages/api/getGeoJson.js
@@ -4,10 +4,10 @@ export default async function geoJson(req, res) {
     if (req.body) {
         const {mapData} = req.body
         try {
-            const result = await axios.get(`http://eu1.locationiq.com/v1/reverse.php?key=${process.env.NEXT_PUBLIC_LOCATIONIQ}&lat=${mapData[0]}&lon=${mapData[1]}&format=json`)
+            const result = await axios.get(`https://eu1.locationiq.com/v1/reverse.php?key=${process.env.NEXT_PUBLIC_LOCATIONIQ}&lat=${mapData[0]}&lon=${mapData[1]}&format=json`)
             res.json(result.data.address.country)
         } catch(e) {
-            res.json({message: e.message})
+            res.json({message: e.message}) 
         }
     }
 }
