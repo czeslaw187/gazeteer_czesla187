1e2b9fffb6a1edbedb47ffe8586ae2f5531f0567
czeslaw187 <66272563+czeslaw187@users.noreply.github.com>
2022-04-15 21:37:14 +0100

    changed axios header at reverse geo call

--- pages/api/getGeoJson.js
+++ pages/api/getGeoJson.js
@@ -4,7 +4,9 @@ export default async function geoJson(req, res) {
     if (req.body) {
         const {mapData} = req.body
         try {
-            const result = await axios.get(`https://api.bigdatacloud.net/data/reverse-geocode-client?latitude=${mapData[0]}&longitude=${mapData[1]}&localityLanguage=en`)
+            const result = await axios.get(`https://api.bigdatacloud.net/data/reverse-geocode-client?latitude=${mapData[0]}&longitude=${mapData[1]}&localityLanguage=en`, {
+                headers: 'Access-Control-Allow-Origin: *'
+            })
             res.json(result.data.countryName)
         } catch(e) {
             res.json({message: e.message})
