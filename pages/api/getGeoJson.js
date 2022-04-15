5496b41bda5c41152a5ce4dfcc65bd8420269cc9
czeslaw187 <66272563+czeslaw187@users.noreply.github.com>
2022-04-15 20:57:21 +0100

    changed reverse geo api

--- pages/api/getGeoJson.js
+++ pages/api/getGeoJson.js
@@ -4,8 +4,8 @@ export default async function geoJson(req, res) {
     if (req.body) {
         const {mapData} = req.body
         try {
-            const result = await axios.get(`http://eu1.locationiq.com/v1/reverse.php?key=${process.env.NEXT_PUBLIC_LOCATIONIQ}&lat=${mapData[0]}&lon=${mapData[1]}&format=json`)
-            res.json(result.data.address.country)
+            const result = await axios.get(`https://api.bigdatacloud.net/data/reverse-geocode-client?latitude=${mapData[0]}&longitude=${mapData[1]}&localityLanguage=en`)
+            res.json(result.data.countryName)
         } catch(e) {
             res.json({message: e.message})
         }
